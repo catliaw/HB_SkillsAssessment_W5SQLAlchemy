3e4e053ade763c39b13b47cdfb6fcfa389c6fd10
catliaw <cat.liaw@gmail.com>
2016-10-30 21:08:07 -0700

    Deleted some extra spaces at the end of query.py

--- query.py
+++ query.py
@@ -106,10 +106,3 @@ def search_brands_by_name(mystr):
 def get_models_between(start_year, end_year):
     
     models_list = Model.query.filter( (Model.year >= start_year) & (Model.year < end_year)).all()
-
-
-
-
-
-
-
